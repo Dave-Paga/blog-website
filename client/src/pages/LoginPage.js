559c15433904bd8d3ddaa86a4e68c13e431b6108
Dave Pagaduan <dave_paga249@yahoo.com.ph>
2023-07-31 22:18:04 +1000

    minor tweaks

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LoginPage() {
   return (
     <div>
-      <form action="">
+      <form className='login'>
         <input type="text" placeholder="username"/>
         <input type="password" placeholder="pasword"/>
         <button>Login</button>
---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function RegisterPage() {
+  return (
+    <form className='register'>
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="pasword"/>
+      <button>Register</button>
+    </form>
+  )
+}
